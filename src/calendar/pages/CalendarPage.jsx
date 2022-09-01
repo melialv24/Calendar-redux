2e54f9d0e50badd7dcf09ea5082d04531577afbe
Melissa <malvarez@datagas.com.co>
2022-09-01 12:45:16 -0500

    personalizando el cuadro de los eventos

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,50 @@
-import React from "react";
-import { Navbar } from "../components";
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { CalendarEvent, Navbar } from "../components";
+import { addHours } from "date-fns";
+import { localizer, getMessagesES } from "../../helpers";
+
+// El evento lo único que pide como obligatorio es el titulo, fecha de inicio y fecha fin
+const events = [
+  {
+    title: "Cumple de melissa",
+    notes: "HAY QUE COMPRAR ALGO",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafa",
+    user: {
+      _id: "123",
+      name: "Melissa",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    const style = {
+      backgroundColor: "#fafa",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return { style };
+  };
+
   return (
     <>
       <Navbar />
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        messages={getMessagesES()}
+        eventPropGetter={eventStyleGetter}
+        components={{ event: CalendarEvent }}
+      />
     </>
   );
 };
